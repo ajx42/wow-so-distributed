109f7fde1419a19001c722f5597716c4e83745af
Aditya Jain <adityajain.aj10@gmail.com>
2023-02-24 00:00:19 -0600

    testing framework, without parallel triggering

--- wowAB/orchestrator.cpp
+++ wowAB/orchestrator.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+
 #include "WowSocket.H"
-
 #include "WowConcert.H"
+#include "argparse/argparse.hpp"
 
-int main() {
-  std::cout << "hello" << std::endl;
-  
-  WowOrchestra perf( 61617 );
- 
-  perf.perform(2);
+int main( int argc, char** argv ) {
+  argparse::ArgumentParser program("orchestrator");
+  program.add_argument( "--port" )
+    .required()
+    .help( "run orchestrator service on this port" );
+
+  program.add_argument( "--iters" )
+    .required()
+    .help( "number of test iterations" );
+
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::runtime_error& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
+  }
+
+  // fetch these as strings, oddly enough the library can't cast
+  // these to int directly
+  auto port = program.get<std::string>( "--port" );
+  auto numIters = program.get<std::string>( "--iters" );
+  auto fName = program.get<std::string>( "--file" );
+
+  WowOrchestra abtesting( std::stoi( port ), fName );
+  abtesting.perform( std::stoi( numIters ) );
 }
